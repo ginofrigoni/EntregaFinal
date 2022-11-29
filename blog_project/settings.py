164c17b9eb2c049bd70f08e5229de74b12ec42b3
ginofrigoni <ginofrigoni@gmail.com>
2022-11-29 17:31:53 -0300

    cambios finales de deploy

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&_!4g*u@%mdz804ri(7+6#&blcg)%g&8ag1$27t1r3av3$lzp-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
